8e5ebd3a8a34494c4bf7940ac3cb94b978b5e9e2
Al Tomizawa <alyssontomizawa@hotmail.com>
2024-02-17 15:01:23 -0300

    revert to previous CORS setup

--- backend/app.js
+++ backend/app.js
@@ -15,19 +15,21 @@ const app = express();
 app.use(bodyParser.json());
 
 //Add CORS middleware
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://localhost:3000',
-  'https://discoverus.fairuse.org',
-  'http://discoverus.fairuse.org',
-  'http://www.discoverus.fairuse.org',
-  'https://www.discoverus.fairuse.org'
-];
+const corsMiddleware = require('./middleware/cors')
+app.use(corsMiddleware);
+// const allowedOrigins = [
+//   'http://localhost:3000',
+//   'https://localhost:3000',
+//   'https://discoverus.fairuse.org',
+//   'http://discoverus.fairuse.org',
+//   'http://www.discoverus.fairuse.org',
+//   'https://www.discoverus.fairuse.org'
+// ];
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
+// app.use(cors({
+//   origin: allowedOrigins,
+//   credentials: true
+// }));
 
 app.options('*', cors()); //make all routes available
 
