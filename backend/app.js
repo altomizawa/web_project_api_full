9c71cbb858ab92e35e07f265de2a4b667715245d
Al Tomizawa <alyssontomizawa@hotmail.com>
2024-02-19 23:25:27 -0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -18,11 +18,10 @@ const app = express();
 app.use(bodyParser.json());
 
 //Add CORS middleware
-// const corsMiddleware = require('./middleware/cors')
-// app.use(corsMiddleware);
+const corsMiddleware = require('./middleware/cors')
+app.use(corsMiddleware);
 
-// app.options('*', cors()); //make all routes available
-app.use(cors())
+app.options('*', cors()); //make all routes available
 
 connectDatabase();
 
